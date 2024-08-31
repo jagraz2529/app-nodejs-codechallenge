6d139ec1675d949d9410f2434f8f33bda8db0774
alejandroagraz <alejandroagraz@gmail.com>
2024-08-31 02:17:31 -0500

    feat(projects): add challenge yape jose agraz

---
+++ projects/yape-micro-server/src/transaction/transaction.service.ts
@@ -0,0 +1,44 @@
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { StripeService } from '../stripe/stripe.service';
+
+@Injectable()
+export class TransactionService {
+  constructor(
+    @Inject('KAFKA_SERVICE')
+    private readonly _clientKafka: ClientKafka,
+    private readonly _stripeService: StripeService,
+  ) {}
+
+  async newTransaction(data: any): Promise<void> {
+    try {
+      if (parseInt(data.amount) < 1000) {
+        const resp = await this._stripeService.generateCharge(
+          data.stripePaymentMethodId,
+          data.stripeCostumerId,
+          parseInt(data.amount),
+          data.currency,
+          data.paymentMethodType,
+        );
+
+        this._clientKafka.emit(
+          'topic.transaction.yape',
+          JSON.stringify({
+            transactionId: data.transactionId,
+            status: resp.status,
+          }),
+        );
+      } else {
+        this._clientKafka.emit(
+          'topic.transaction.yape',
+          JSON.stringify({
+            transactionId: data.transactionId,
+            status: 'rejected',
+          }),
+        );
+      }
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
---
+++ projects/yape-server/src/database/database.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DATABASE_HOST'),
+        port: parseInt(configService.get('DATABASE_PORT')),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: false,
+        extra: {
+          max: 10,
+          min: 2,
+          idleTimeoutMillis: 30000,
+        },
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
